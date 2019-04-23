b61273a5acec23577ba0ce8a298f0f39d80091d7
Matty Baba Allos <matty.babaallos@gmail.com>
2019-04-23 10:26:24 -0700

    added more coverge

--- CSM_coverage.sv
+++ CSM_coverage.sv
@@ -1,6 +1,6 @@
 module coverage(CSM_bfm bfm);
 
-   covergroup ops;
+   covergroup singals @(negedge bfm.clk);
 		A_rw : coverpoint bfm.A_rw {
 			bins read = {'0};
 			bins write = {'1};
@@ -27,16 +27,63 @@ module coverage(CSM_bfm bfm);
 			}
 
 		B_enable : coverpoint bfm.B_enable{
-			bins A_disable = {'0};
-			bins A_enable = {'1};
+			bins B_disable = {'0};
+			bins B_enable = {'1};
 			}
+
+		A_hold : coverpoint bfm.A_hold;
+		B_hold : coverpoint bfm.B_hold;
+
+		A_release : coverpoint bfm.A_release;
+		B_release : coverpoint bfm.B_release;
 
 		A_err : coverpoint bfm.A_err;
 		B_err : coverpoint bfm.B_err;
 
-		// A_ops : cross A_rw, A_enable, A_ack{
-		// 	bin read = {A_rw.read, A_enable.A_enable,A_ack.no_ack};
-		// }
+
+		A_in_AD : coverpoint bfm.A_in_AD{
+				bins address_data[4] = {[0:$]};
+		}
+
+		B_in_AD : coverpoint bfm.B_in_AD{
+				bins address_data[4] = {[0:$]};
+		}
+
+		A_out_data : coverpoint bfm.A_out_data{
+				bins dataout[4] = {[0:$]};
+		}
+
+		B_out_data : coverpoint bfm.B_out_data{
+				bins dataout[4] = {[0:$]};
+		}
 
 	endgroup
+
+	covergroup ops @(negedge bfm.clk);
+		A_read: coverpoint { bfm.A_enable , bfm.A_rw , bfm.A_ack}{
+			bins read = {'b100};
+		}
+
+		B_read: coverpoint { bfm.B_enable , bfm.B_rw , bfm.B_ack}{
+			bins read = {'b100};
+		}
+
+
+		A_write: coverpoint { bfm.A_enable , bfm.A_rw , bfm.A_ack}{
+			bins write = {'b110};
+		}
+
+
+		B_write: coverpoint { bfm.B_enable , bfm.B_rw , bfm.B_ack}{
+			bins write = {'b110};
+		}
+
+		A_read_B_read: cross A_read, B_read;
+		A_read_B_write: cross A_read,B_write;
+		A_write_B_write: cross A_write, B_write;
+	endgroup
+
+	singals singals_cov = new;
+	ops ops_cov = new;
+
 endmodule
